6a5d6c35f99e38467ae1b0de34d768808a198078
root <924797163@qq.com>
2018-05-05 17:21:01 +0800

    添加404页面

--- terrorism_rear_end/settings.py
+++ terrorism_rear_end/settings.py
@@ -127,6 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = '/var/www/static/terrorism_rear_end/'
+STATIC_ROOT = '/var/www/script/static/terrorism_rear_end/'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/terrorism_rear_end/'

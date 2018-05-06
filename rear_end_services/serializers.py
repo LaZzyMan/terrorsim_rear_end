71bddc8ee5fe4779563d69d32e68e32e84e866c7
924797163@qq.com <924797163@qq.com>
2018-05-07 00:35:44 +0800

    配置DRF

--- rear_end_services/serializers.py
+++ rear_end_services/serializers.py
@@ -1,11 +1,42 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from .models import TerrorismData
+from rest_framework import serializers
+from .models import TerrorismData, Country, Attack, Weapon, Region, Target
 
+class TDInfoSerialier(serializers.ModelSerializer):
+    class Meta:
+        model = TerrorismData
+        fields = ('id', 'year', 'month', 'day', 'date', 'city',
+        'summary', 'suicide', 'group_name', 'motive', 'num_kill',
+        'num_wound', 'prop_value', 'prop_comment', 'country_id',
+        'region_id', 'attack_type', 'target_type', 'weapon_type')
 
-class TDSerializer(GeoFeatureModelSerializer):
+class TDGeoSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = TerrorismData
         geo_field = 'location'
-        id_field = False
-        auto_field = True
-        fields = ('id', 'location', 'date', 'country_id', 'region_id')
+        id_field = 'id'
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('country_id', 'country_name')
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ('region_id', 'region_name')
+
+class AttackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attack
+        fields = ('attack_type_id', 'attack_type_name')
+
+class TargetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Target
+        fields = ('target_type_id', 'target_type_name')
+
+class WeaponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weapon
+        fields = ('weapon_type_id', 'weapon_type_name')
--- rear_end_services/urls.py
+++ rear_end_services/urls.py
@@ -1,9 +1,23 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import terrorism_info
+from rear_end_services import views
+
+router = DefaultRouter()
+router.root_view_name = 'API List'
+router.register(r'tdinfo', views.TDInfoViewSet)
+router.register(r'country', views.CountryViewSet)
+router.register(r'region', views.RegionViewSet)
+router.register(r'attack', views.AttackViewSet)
+router.register(r'weapon', views.WeaponViewSet)
+router.register(r'target', views.TargetViewSet)
+
+country_list = views.CountryViewSet.as_view({
+    'get': 'list'
+})
 
 urlpatterns = [
-    url(r'terrorism/(?P<id>[0-9]+)/$', terrorism_info),
+    url(r'^', include(router.urls)),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
